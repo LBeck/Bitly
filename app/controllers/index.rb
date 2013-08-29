99b3dccf6283265a61d174c442a76fc93b10f49e
DBC-Apprentice <dbc@dbc.com>
2013-08-28 21:49:17 -0700

    add analytics

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,14 +1,20 @@
 
 # let user create new short URL, display a list of shortened URLs
 get '/' do
+
   erb :index
 end
 
 post '/urls' do
   @long_url= params[:long_url]
-  url_row = Url.create(long_url: @long_url)
-  @extension = url_row.shortened_url
-  erb :display
+  url_row = Url.new(long_url: @long_url)
+  if url_row.save
+    @extension = url_row.shortened_url
+    erb :display
+  else
+    @errors = url_row.errors.messages
+    erb :index
+  end
 end
 
 
@@ -21,4 +27,19 @@ get '/:short_url' do
  
 end
 
+get '/analytics/:short_url' do
 
+@click_count = Url.find_by_shortened_url(params[:short_url]).click_count
+
+erb :analytics 
+end 
+
+
+
+
+get '/analytics' do
+
+
+
+erb :analytics
+end
